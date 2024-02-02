c26e8684aa1ce34e2c46012ce2d31edaaed82362
Guillot Tony <tony.guillot@protonmail.com>
2024-02-02 18:06:06 +0100

    Updated LICENCE and incremeted year

--- include/phoenix/structs/bitmap.h
+++ include/phoenix/structs/bitmap.h
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2023 Guillot Tony <tony.guillot@protonmail.com>
+ * Copyright © 2024 Guillot Tony <tony.guillot@protonmail.com>
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
